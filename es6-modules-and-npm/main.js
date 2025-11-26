7239bb95faf890553fc127b38919ac72809f1983
deepa57 <deepagurung55662@gmail.com>
2025-11-26 23:17:42 +0545

    Solved q3: Implemented namespace import

--- es6-modules-and-npm/main.js
+++ es6-modules-and-npm/main.js
@@ -1,8 +1,12 @@
 import { add, sub } from './math.js';
 import g from './greet.js';
+import * as utils from './utils.js';
 console.log(
     add(6,7), 
     sub(9,5)
 );
 
 console.log(g('deepa'));
+
+console.log(utils.getdata());
+console.log(utils.getcountry("Nepal"));
---
+++ es6-modules-and-npm/utils.js
@@ -0,0 +1,9 @@
+export function getdata()
+{
+    return "today date: 11/26/2025";
+}
+
+export function getcountry(country)
+{
+    return `${country}`;
+}
