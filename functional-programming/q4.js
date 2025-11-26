5e7f2bc984dd69dd91067a7964aefd9023e517f3
deepa57 <deepagurung55662@gmail.com>
2025-11-26 22:32:33 +0545

    Solved q3: Implemented function for find by ID using find method

---
+++ functional-programming/q4.js
@@ -0,0 +1,10 @@
+const users = [
+  { id: 1, name: "A" },
+  { id: 2, name: "B" },
+  { id: 3, name: "C" }
+];
+
+function findById(arr, id) {
+  return arr.find(x => x.id === id);
+}
+console.log(findById(users, 2));
