6c0834b08ef5847022aad4e80a97caef004e75cb
deepa57 <deepagurung55662@gmail.com>
2025-11-26 22:26:11 +0545

    Solved q1: Implemented filter method using filter()

---
+++ functional-programming/q2.js
@@ -0,0 +1,10 @@
+function  filterAdults(users)
+{
+    return users.filter( user => user.age>= 18);
+}
+
+const user = [
+    {name:"A", age:15}, 
+    {name:"B", age:25}
+];
+console.log(filterAdults(user));
