7f43fcb799168e839754ebc44b2f372b89381d2c
deepa57 <deepagurung55662@gmail.com>
2025-11-27 21:33:14 +0545

    Solved q4: Implemented function for parallel fetch using promise.all

---
+++ rest-api/q4.js
@@ -0,0 +1,11 @@
+async function fetchPostsAndComments() {
+  const [posts, comments] = await Promise.all([
+    fetch('https://jsonplaceholder.typicode.com/posts').then(r=>r.json()),
+    fetch('https://jsonplaceholder.typicode.com/comments').then(r=>r.json())
+  ]);
+  return { posts, comments };
+}
+
+fetchPostsAndComments().then(result =>{
+    console.log(result)
+});
